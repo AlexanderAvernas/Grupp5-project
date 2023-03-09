af73993955462a19adb3c7083a0983fe9e23fa7b
amariths <amar.mehdi@iths.se>
2023-03-09 09:23:19 +0100

    färdig quiz

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -73,10 +73,26 @@ body {
     height: 400px;
   }
 
-  #col {
+  #Quizdefault h2 {
+    font-family: amiko;
+    font-weight: 400;
+    font-size: 35px;
+    color: white;
+    position: relative;
+    text-align: center;
+    top: 50px;
+  }
+  #Quizdefault h3 {
+    font-family: amiko;
+    font-weight: 400;
+    font-size: 18px;
+    color: white;
+    position: relative;
+    top: 50px;
+    text-align: center;
   }
 
-  #cardquiz {
+  #Quizdefault {
   }
 
   #quiztext > a {
@@ -111,9 +127,6 @@ body {
 
   #quiztext > a:active {
     background-color: #6d3d27;
-  }
-  #Quizdefault {
-    margin-left: 100px;
   }
 
   #button1 {
@@ -152,7 +165,7 @@ body {
 @media screen and (max-width: 520px) {
   #card {
     width: auto;
-    height: 550px;
+    height: 700px;
   }
 
   #quiztext {
@@ -200,6 +213,8 @@ body {
     align-items: center;
     color: white;
     font-size: 20px;
+    position: relative;
+    top: 30px;
   }
 
   #quiztext > a {
@@ -229,6 +244,8 @@ body {
   }
   #quiztext > h4 {
     grid-row-start: 6;
+    position: relative;
+    bottom: 15px;
   }
   #quiztext > h6 {
     color: #fff;
@@ -236,6 +253,8 @@ body {
     text-align: center;
     margin-right: auto;
     margin-top: auto;
+    position: relative;
+    bottom: 15px;
   }
 }
 </style>
